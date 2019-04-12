ee6d92d63e71f0f3e0a6acafd29fdba3233d5582
wdunn <williamdunn195@gmail.com>
2019-04-11 21:50:24 -0400

    Add push, pop, peek

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,8 +1,18 @@
 class Stack
+    attr_reader :stack
     def initialize
         @stack = []
     end
 
     def push(el)
+        @stack << el
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
     end
 end
