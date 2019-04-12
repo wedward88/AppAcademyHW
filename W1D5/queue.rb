2dfb424a4d148f79b49ab28f8c35aea60c997e1a
wdunn <williamdunn195@gmail.com>
2019-04-11 21:55:48 -0400

    add queue.rb and create instance methods

---
+++ W1D5/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue.first
+    end
+end
